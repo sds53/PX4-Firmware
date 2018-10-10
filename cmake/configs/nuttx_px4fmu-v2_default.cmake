9b8ddd33f281b3b1a5f68c4f25cf9f54252352c0
Zachary Taylor <zacharyjeremytaylor@gmail.com>
2018-10-10 13:18:44 +0200

    lower imu rate and added telem

--- cmake/configs/nuttx_px4fmu-v2_default.cmake
+++ cmake/configs/nuttx_px4fmu-v2_default.cmake
@@ -19,7 +19,7 @@ set(config_module_list
 	#drivers/barometer/bmp280
 	#drivers/bst
 	drivers/camera_trigger
-	#drivers/frsky_telemetry
+	drivers/telemetry/frsky_telemetry
 	drivers/gps
 	#drivers/hott
 	#drivers/iridiumsbd
@@ -36,7 +36,7 @@ set(config_module_list
 	#drivers/protocol_splitter
 	drivers/pwm_input
 	#drivers/pwm_out_sim
-	drivers/px4flow
+	#drivers/px4flow
 	drivers/px4fmu
 	drivers/px4io
 	drivers/rgbled
