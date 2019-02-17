9a2320c6d6deeb928180aa3b4a2d1c1c85387c8a
Sam Schofield <sds53@uclive.ac.nz>
2019-02-18 12:09:57 +1300

    Removed ASL offboard yawrate changes

--- src/modules/mc_att_control/mc_att_control_main.cpp
+++ src/modules/mc_att_control/mc_att_control_main.cpp
@@ -379,12 +379,12 @@ MulticopterAttitudeControl::control_attitude(float dt)
 	Quatf q(_v_att.q);
 	Quatf qd(_v_att_sp.q_d);
 
-	 // HACKING FROM HERE ON
-	if (_v_control_mode.flag_control_offboard_enabled) {
-	  Eulerf current_euler(q);
-	  Eulerf desired_euler(qd);
-	  qd = Quatf(Eulerf(desired_euler.phi(), desired_euler.theta(), current_euler.psi()));
-	}
+//	 // HACKING FROM HERE ON
+//	if (_v_control_mode.flag_control_offboard_enabled) {
+//	  Eulerf current_euler(q);
+//	  Eulerf desired_euler(qd);
+//	  qd = Quatf(Eulerf(desired_euler.phi(), desired_euler.theta(), current_euler.psi()));
+//	}
 
 	/* ensure input quaternions are exactly normalized because acosf(1.00001) == NaN */
 	q.normalize();
@@ -448,11 +448,11 @@ MulticopterAttitudeControl::control_attitude(float dt)
 		}
 	}
 
-	// ugly hack to get yaw rate
-    if (_v_control_mode.flag_control_offboard_enabled) {
-		vehicle_rates_setpoint_poll();
-		_rates_sp(2) = _v_rates_sp.yaw;
-    }
+//	// ugly hack to get yaw rate
+//    if (_v_control_mode.flag_control_offboard_enabled) {
+//		vehicle_rates_setpoint_poll();
+//		_rates_sp(2) = _v_rates_sp.yaw;
+//    }
 
 	/* VTOL weather-vane mode, dampen yaw rate */
 	if (_vehicle_status.is_vtol && _v_att_sp.disable_mc_yaw_control) {
@@ -708,15 +708,22 @@ MulticopterAttitudeControl::run()
 				_v_rates_sp.thrust = _thrust_sp;
 				_v_rates_sp.timestamp = hrt_absolute_time();
 
-				// Don't republish if in offboard mode, since we send the yaw rate here.
-		        if (!_v_control_mode.flag_control_offboard_enabled) {
-		  				if (_v_rates_sp_pub != nullptr) {
-		  					orb_publish(_rates_sp_id, _v_rates_sp_pub, &_v_rates_sp);
+                if (_v_rates_sp_pub != nullptr) {
+					orb_publish(_rates_sp_id, _v_rates_sp_pub, &_v_rates_sp);
 
-		  				} else if (_rates_sp_id) {
-		  					_v_rates_sp_pub = orb_advertise(_rates_sp_id, &_v_rates_sp);
-		  				}
-		        }
+				} else if (_rates_sp_id) {
+					_v_rates_sp_pub = orb_advertise(_rates_sp_id, &_v_rates_sp);
+				}
+
+//				// Don't republish if in offboard mode, since we send the yaw rate here.
+//		        if (!_v_control_mode.flag_control_offboard_enabled) {
+//		  				if (_v_rates_sp_pub != nullptr) {
+//		  					orb_publish(_rates_sp_id, _v_rates_sp_pub, &_v_rates_sp);
+//
+//		  				} else if (_rates_sp_id) {
+//		  					_v_rates_sp_pub = orb_advertise(_rates_sp_id, &_v_rates_sp);
+//		  				}
+//		        }
 			} else {
 				/* attitude controller disabled, poll rates setpoint topic */
 				if (_v_control_mode.flag_control_manual_enabled) {
